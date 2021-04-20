d9e1bb91d14c5a95661a8e2b389dc70d018340db
Thales Gomes <thalesrgomes@gmail.com>
2021-04-19 22:05:32 -0300

    create usuarios

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+const { request } = require("express");
 const express = require("express");
+const app = express();
+
+app.use(express.json());
 
 let usuarios = [
     {nome: "Thales",
@@ -6,15 +10,29 @@ let usuarios = [
     }
 ];
 
-const app = express();
-
 app.get('/inicio',(request, response) =>{
     return response.send('Olá mundo!');
 })
 
 app.get('/usuarios', (request, response) =>{
+
     return response.json(usuarios);
 })
+
+app.post('/usuarios', (request, response) =>{
+
+   const {nome, sobrenome} = request.body;
+
+   usuarios.push({nome, sobrenome});
+
+   return response.json({nome, sobrenome})
+})
+
+app.put('/usuarios/:nome', (request, response) =>{
+    const { nome } = request.params;
+    
+})
+
 
 app.listen(3000,()=>{
     console.log('Servidor rodando!');
